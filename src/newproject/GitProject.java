dacb98bbfbe52c846a9ed4a0108fe7d38feba556
ManikandanRamar <gaylemani175@gmail.com>
2022-12-29 21:53:13 +0530

    B Works

--- src/newproject/GitProject.java
+++ src/newproject/GitProject.java
@@ -4,6 +4,8 @@ public class GitProject {
 	public static void main(String[] args) {
 		String s = "Mani";
 		System.out.println("Name is :"+ s);
+		char charAt = s.charAt(2);
+		System.out.println(charAt);
 		
 	}
 
